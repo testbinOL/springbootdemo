b116b4c45a99bd0733bce2e313214bb424b85c72
testbinOL <xb1989dyx@163.com>
2018-06-11 16:00:09 +0800

    add greetingcontroller for requestmethod test

---
+++ src/main/java/com/bin/demo/controller/GreetingController.java
@@ -0,0 +1,20 @@
+package com.bin.demo.controller;
+
+import com.bin.demo.domain.Greeting;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+@RestController
+public class GreetingController {
+
+    private static final String template = "Hello,%s!";
+    private final AtomicInteger counter = new AtomicInteger();
+
+    @RequestMapping("/greeting")
+    public Greeting greeting(@RequestParam(value = "name", defaultValue = "world") String name) {
+        return new Greeting(counter.incrementAndGet(), String.format(template, name));
+    }
+}
